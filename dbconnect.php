49a6e022eef6dac5409df60934ccbe09912dbb79
Vaishali Kirtikumar Nile <80026547+VAISHALI-NILE@users.noreply.github.com>
2024-03-23 16:30:08 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$conn = new mysqli("sql100.infinityfree.com", "if0_34678114", "943Uw88q1QdrSMC","if0_34678114_serenebeauty") or die("Connect failed: %s\n". $conn -> error);
+$conn = new mysqli("sql107.infinityfree.com", "if0_36221387", "A39wg4zrYDpLcT","if0_36221387_serenebeauty") or die("Connect failed: %s\n". $conn -> error);
 $_SESSION['conn'] = $conn;
 $_SESSION['flag'] = 0;
 ?>
